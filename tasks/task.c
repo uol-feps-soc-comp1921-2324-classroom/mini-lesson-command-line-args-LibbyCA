b799ed84a235ee9b5cf9bdf1cf5c9bcf8d64590f
LibbyCA <146595630+LibbyCA@users.noreply.github.com>
2024-02-13 18:48:56 +0000

    completed

--- tasks/task.c
+++ tasks/task.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 /**
  * argv[1] - num1
@@ -6,18 +8,26 @@
 */
 
 
-int main(int argc, char *argv) {
+int main(int argc, char *argv[]) {
     //int num1, num2;
+
+    if (argc != 3)
+    {
+        printf("Usage t <sum>\n");
+        return 0;
+    }
 
     // Prompt the user for input
     //printf("Enter the first number: ");
     //scanf("%d", &num1);
+    int num1 = atoi(argv[1]);
 
     //printf("Enter the second number: ");
     //scanf("%d", &num2);
+    int num2 = atoi(argv[2]);
 
     // Calculate and print the sum
-    int sum = argv[1] + argv[2];
+    int sum = num1 + num2;
     printf("Sum: %d\n", sum);
 
     return 0; // Exit successfully
