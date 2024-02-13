9d00d2dbae494d0e136bfd47ace5f7329a3f059f
LibbyCA <146595630+LibbyCA@users.noreply.github.com>
2024-02-13 18:43:48 +0000

    started task

--- tasks/task.c
+++ tasks/task.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    int num1, num2;
+/**
+ * argv[1] - num1
+ * argv[2] - num2
+*/
+
+
+int main(int argc, char *argv) {
+    //int num1, num2;
 
     // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    //printf("Enter the first number: ");
+    //scanf("%d", &num1);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    //printf("Enter the second number: ");
+    //scanf("%d", &num2);
 
     // Calculate and print the sum
-    int sum = num1 + num2;
+    int sum = argv[1] + argv[2];
     printf("Sum: %d\n", sum);
 
     return 0; // Exit successfully
